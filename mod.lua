0138c3ea0cdb9aa89bc0ad40d1cd1dd059fcb691
Florian Thiel <flth@flth.de>
2023-12-03 15:26:11 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,43 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = 'jay_',
+				role = 'CREATOR',
+				text = 'Modell',
+				tfnetId = 28954,
+			},
+			{
+				name = "Bko@dview",
+				role = "CREATOR",
+				tfnetId = '42693',
+			},
+			{
+		        name = "bandion",
+		        role = "CREATOR",
+		    },
+			{
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },  
+		},
+		tags = { "cars", "dependency mod", "car cargo set", },
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+	
+	runFn = function (settings)
+	
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,18 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Car Cargo Pack",
+		["mod_desc"] = "Dependency mod including car models.",
+		
+		
+	},
+
+	de = {
+
+		["mod_name"] = "Car Cargo Pack",
+		["mod_desc"] = "Abhängigkeitsmod, welcher Autos zur Beladung enthält.",
+        
+	},
+}
+end
